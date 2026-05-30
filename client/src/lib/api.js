d76f4bfb20ad3aa2a8b553936276e4d66a5c365e
Niraj Joshi <infocodewithniraj@gmail.com>
2026-05-30 22:13:33 +0545

    url

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.PROD
+  ? '/api/v1'
+  : import.meta.env.VITE_API_URL || '/api/v1';
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api/v1',
+  baseURL,
   withCredentials: true, // send cookies automatically
 });
 
