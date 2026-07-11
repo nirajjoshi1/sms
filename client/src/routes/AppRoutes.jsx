696164b40bb9145f6b8c325932b31a95714e7d57
Jsbyte <143607256+nirajjoshi1@users.noreply.github.com>
2026-07-12 00:05:14 +0545

    Restrict Super Admin from school tenant routes

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -151,7 +151,7 @@ const AppRoutes = () => {
         <Route index element={isAuthenticated ? <Dashboard /> : null} />
 
         {/* Student Routes */}
-        <Route element={<ProtectedRoute allowedRoles={['SUPER_ADMIN', 'ADMIN', 'RECEPTIONIST']} />}>
+        <Route element={<ProtectedRoute allowedRoles={['ADMIN', 'RECEPTIONIST']} />}>
           <Route path="students" element={<StudentList />} />
           <Route path="students/:id" element={<StudentProfile />} />
           <Route path="students/admission" element={<StudentAdmission />} />
@@ -162,14 +162,14 @@ const AppRoutes = () => {
         </Route>
 
         {/* Staff Routes */}
-        <Route element={<ProtectedRoute allowedRoles={['SUPER_ADMIN', 'ADMIN']} />}>
+        <Route element={<ProtectedRoute allowedRoles={['ADMIN']} />}>
           <Route path="staff" element={<StaffList />} />
           <Route path="staff/add" element={<StaffAdd />} />
           <Route path="staff/edit/:id" element={<StaffEdit />} />
         </Route>
 
         {/* Academics Routes */}
-        <Route element={<ProtectedRoute allowedRoles={['SUPER_ADMIN', 'ADMIN']} />}>
+        <Route element={<ProtectedRoute allowedRoles={['ADMIN']} />}>
           <Route path="academics/class" element={<Classes />} />
           <Route path="academics/sections" element={<Sections />} />
           <Route path="academics/subjects" element={<Subjects />} />
@@ -181,7 +181,7 @@ const AppRoutes = () => {
         </Route>
 
         {/* HR Routes */}
-        <Route element={<ProtectedRoute allowedRoles={['SUPER_ADMIN', 'ADMIN', 'ACCOUNTANT']} />}>
+        <Route element={<ProtectedRoute allowedRoles={['ADMIN', 'ACCOUNTANT']} />}>
           <Route path="hr/staff-directory" element={<StaffDirectory />} />
           <Route path="hr/disabled-staff" element={<DisabledStaff />} />
           <Route path="hr/attendance" element={<StaffAttendance />} />
@@ -195,7 +195,7 @@ const AppRoutes = () => {
         </Route>
 
         {/* Fees Routes */}
-        <Route element={<ProtectedRoute allowedRoles={['SUPER_ADMIN', 'ADMIN', 'ACCOUNTANT', 'RECEPTIONIST']} />}>
+        <Route element={<ProtectedRoute allowedRoles={['ADMIN', 'ACCOUNTANT', 'RECEPTIONIST']} />}>
           <Route path="fees/collect" element={<CollectFees />} />
           <Route path="fees/search" element={<SearchFees />} />
           <Route path="fees/due" element={<DueFees />} />
@@ -209,7 +209,7 @@ const AppRoutes = () => {
         </Route>
 
         {/* Income Routes */}
-        <Route element={<ProtectedRoute allowedRoles={['SUPER_ADMIN', 'ADMIN', 'ACCOUNTANT']} />}>
+        <Route element={<ProtectedRoute allowedRoles={['ADMIN', 'ACCOUNTANT']} />}>
           <Route path="income/head" element={<IncomeHead />} />
           <Route path="income/add" element={<AddIncome />} />
           <Route path="income/edit/:id" element={<AddIncome />} />
@@ -217,7 +217,7 @@ const AppRoutes = () => {
         </Route>
 
         {/* Expense Routes */}
-        <Route element={<ProtectedRoute allowedRoles={['SUPER_ADMIN', 'ADMIN', 'ACCOUNTANT']} />}>
+        <Route element={<ProtectedRoute allowedRoles={['ADMIN', 'ACCOUNTANT']} />}>
           <Route path="expenses/head" element={<ExpenseHead />} />
           <Route path="expenses/add" element={<AddExpense />} />
           <Route path="expenses/edit/:id" element={<AddExpense />} />
@@ -225,7 +225,7 @@ const AppRoutes = () => {
         </Route>
 
         {/* Certificate Routes */}
-        <Route element={<ProtectedRoute allowedRoles={['SUPER_ADMIN', 'ADMIN']} />}>
+        <Route element={<ProtectedRoute allowedRoles={['ADMIN']} />}>
           <Route path="certificates/student-certificate" element={<StudentCertificate />} />
           <Route path="certificates/generate-certificate" element={<GenerateCertificate />} />
           <Route path="certificates/transfer-certificate" element={<TransferCertificate />} />
@@ -236,7 +236,7 @@ const AppRoutes = () => {
         </Route>
 
         {/* CMS Routes */}
-        <Route element={<ProtectedRoute allowedRoles={['SUPER_ADMIN', 'ADMIN']} />}>
+        <Route element={<ProtectedRoute allowedRoles={['ADMIN']} />}>
           <Route path="cms/event" element={<Events />} />
           <Route path="cms/gallery" element={<Gallery />} />
           <Route path="cms/news" element={<News />} />
@@ -247,7 +247,7 @@ const AppRoutes = () => {
         </Route>
 
         {/* Settings Routes */}
-        <Route element={<ProtectedRoute allowedRoles={['SUPER_ADMIN', 'ADMIN']} />}>
+        <Route element={<ProtectedRoute allowedRoles={['ADMIN']} />}>
           <Route path="settings/general" element={<GeneralSetting />} />
           <Route path="settings/session" element={<SessionSetting />} />
           <Route path="settings/notification" element={<NotificationSetting />} />
